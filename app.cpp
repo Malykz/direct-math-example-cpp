ccc808209c05a0c68a83d01d2560f3bc8033dc36
luthfi <luthlirik@gmail.com>
2025-03-27 11:24:42 +0700

    Latihan Dasar

--- app.cpp
+++ app.cpp
@@ -1,29 +1,55 @@
 #include <iostream>
 #include "src/Math.h"
 #include <map>
+#include <optional>
+#include <variant>
+
 using namespace std;
 
-vector <int> super(int target, vector <int> maven)
+int timeli(vector <int> ls)
 {
-    map <int, int> super;
-    vector <int> result;
+    int ba = ls[0];
+    for(size_t a = 1; a < ls.size(); a++) ba *= ls[a];
+    return ba;
+}
 
-    int idx = 0;
-    for ( int s : maven)
+map <int, vector <int>> super(
+    vector <int> maven,
+    size_t len,
+    map <int, vector <int>> result = {}
+) {
+    vector <int> rem;
+    map <int, int> index;
+    auto asd = 0;
+
+    for (int i : maven) index[maven[i]] = i;
+    for (size_t asd = 0; asd < maven.size(); asd++)
     {
-        super[s] = idx;
-        idx += 1;
-    } 
-    
-    for ( int a : maven ) for (int b : maven) if (a * b == target) result = {super[b], super[a]};
+        
+        rem = maven;
+        rem.erase(rem.begin() + asd);
+        result[timeli(rem)] = rem;
+
+        if (rem.size() > len) {
+            result = super(rem, len, result);
+        }
+    }
 
     return result;
+    
+}
+
+optional <vector <int>> brute(int target, vector <int> maven, size_t len)
+{
+    map <int, vector <int>> spr = super(maven, len);
+    if(spr[target].size() != len) return nullopt;
+    return spr[target];
 }
 
 int main() {
-    for (int a : super(15, {1, 2, 5, 3}))
-    {
-        cout << a;
-    }
+    vector <int> mvn = {1, 2, 3, 4, 5};
+    auto asd = brute(6, mvn, 2);
+    if (asd) for (int a : asd.value()) cout << a;
+    
     return 0;
 }
